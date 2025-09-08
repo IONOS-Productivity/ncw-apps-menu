a4e79490baec5fff76a9ed8898731db3fdb8c14f
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-09-08 17:25:24 +0200

    refactor: rename AppTemplate namespace and references to NcwAppsMenu
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace OCA\AppTemplate\Controller;
+namespace OCA\NcwAppsMenu\Controller;
 
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\ApiRoute;
