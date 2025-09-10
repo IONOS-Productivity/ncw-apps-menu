6febbdd601b4476d901c98d96179a3593239b54e
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-09-10 09:56:24 +0200

    refactor: move Apps to AppsMenu component
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
+import AppsMenu from './lib/view/AppsMenu.vue'
 Vue.mixin({ methods: { t, n } })
 
-const View = Vue.extend(App)
+const View = Vue.extend(AppsMenu)
 new View().$mount('#ncw_apps_menu')
